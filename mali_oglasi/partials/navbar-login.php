3164ac1edecb379b83369664ece17eaa813041c8
Aleksandar <77167474+acke123@users.noreply.github.com>
2021-02-23 13:15:56 +0100

    Delete mali_oglasi directory

--- mali_oglasi/partials/navbar-login.php
+++
@@ -1,4 +0,0 @@
-<ul class="navbar-nav ms-auto">
-<li class="nav-item"><a href="login.view.php" class="nav-link">Login</a></li>
-<li class="nav-item"><a href="register.view.php" class="nav-link">Register</a></li>
-</ul>
